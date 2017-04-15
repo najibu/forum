49dbf3cde4038ce7e9ad71c8eae8b2d1e5021d01
najibu <ns.najibu@gmail.com>
2017-04-15 13:44:49 +0100

    Added test a user can browse threads

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Thread;
 use Illuminate\Http\Request;
 
-class ThreadController extends Controller
+class ThreadsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,8 +13,10 @@ class ThreadController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
+    {   
+        $threads = Thread::latest()->get();
+
+        return view('threads.index', compact('threads'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/threads', 'ThreadsController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
