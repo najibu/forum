3d1a169291558badd9e1a6f61bde675f7e36aa59
najibu <ns.najibu@gmail.com>
2017-04-15 15:44:47 +0100

    Added reply test

--- app/Reply.php
+++ app/Reply.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use App\Thread;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function thread()
+    {
+      return $this->belongsTo(Thread::class);
+    }
+
+    public function owner()
+    {
+      return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- app/Thread.php
+++ app/Thread.php
@@ -2,12 +2,23 @@
 
 namespace App;
 
+use App\Reply;
 use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
-{
+{ 
+    /**
+     * Fetch a path to the current thread.
+     * 
+     * @return string
+     */
     public function path()
     {
       return '/threads/' . $this->id;
+    }
+
+    public function replies()
+    {
+      return $this->hasMany(Reply::class);
     }
 }
