63900c7f61af7aac4a2032859a15c847071796ee
najibu <ns.najibu@gmail.com>
2017-04-15 16:16:09 +0100

    Added thread creator test

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -11,11 +11,22 @@ class ThreadTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp()
+    {
+      parent::setUp();
+
+      $this->thread = factory('App\Thread')->create();
+    }
+
     /** @test  */
     function a_thread_has_replies()
     {
-      $thread = factory('App\Thread')->create();
-
-      $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $thread->replies);
+      $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
     }
+
+    /** @test  */
+    function a_thread_has_a_creator()
+    {
+      $this->assertInstanceOf('App\User', $this->thread->creator);
+    } 
 }
