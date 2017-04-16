6564418aa6d8ec280f92d5396e492ab00b838a97
najibu <ns.najibu@gmail.com>
2017-04-16 14:24:53 +0100

    Added reply form

--- tests/Feature/ParticipateInThreadTest.php
+++ tests/Feature/ParticipateInThreadTest.php
@@ -7,17 +7,17 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 
-class ParticipateInForumTest extends TestCase
+class ParticipateInThreadTest extends TestCase
 {
     use DatabaseMigrations;
 
-    /** @test  */
+    /** @test */
     function unauthenticated_users_may_not_add_replies()
     {
-      $this->expectException('Illuminate\Auth\AuthenticationException');
+        $this->expectException('Illuminate\Auth\AuthenticationException');
 
-      $this->post('/threads/1/replies', []);
-    } 
+        $this->post('/threads/1/replies', []);
+    }
 
     /** @test  */
     function an_authenticated_user_may_participate_in_forum_threads()
