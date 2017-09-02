02b5a1897803f2ef4dfff5c672299d82b20c4076
najibu <ns.najibu@gmail.com>
2017-09-02 13:14:21 +0100

    Validation errors and old data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,6 +44,11 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         <li><a href="/threads">All Threads</a></li>
+
+                        <li>
+                            <a href="/threads/create">New Thread</a>
+                        </li>
+
                         <li class="dropdown">
                           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Channels <span class="caret"></span></a>
 
@@ -53,6 +58,7 @@
                             @endforeach
                           </ul>
                         </li>
+
                     </ul>
 
                     <!-- Right Side Of Navbar -->
