e6999f58fe265fba2c876b8aba489037a36d7446
najibu <ns.najibu@gmail.com>
2017-09-03 13:01:07 +0100

    Meta Details and Pagination

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -67,7 +67,10 @@ class ThreadsController extends Controller
      */
     public function show($channelId, Thread $thread)
     {
-        return view('threads.show', compact('thread'));
+        return view('threads.show', [
+            'thread' => $thread,
+            'replies' => $thread->replies()->paginate(1)
+        ]);
     }
 
     /**
