c001dad920384a9b7c9a8bf7d5b9579872b3b830
najibu <ns.najibu@gmail.com>
2017-09-03 12:23:33 +0100

    refactoring threads controller

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Channel;
+use App\Filters\ThreadFilters;
 use App\Thread;
-use App\User;
 use Illuminate\Http\Request;
 
 class ThreadsController extends Controller
@@ -18,21 +18,9 @@ class ThreadsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Channel $channel)
+    public function index(Channel $channel, ThreadFilters $filters)
     {
-        if ($channel->exists) {
-            $threads = $channel->threads()->latest();
-        } else {
-            $threads = Thread::latest();
-        }
-
-        // if request('by'), we should filter by the given username.
-        if ($username = request('by')) {
-            $user = User::where('name', $username)->firstOrFail();
-            $threads->where('user_id', $user->id);
-        }
-
-        $threads = $threads->get();
+        $threads = $this->getThreads($channel, $filters);
 
         return view('threads.index', compact('threads'));
     }
@@ -113,5 +101,18 @@ class ThreadsController extends Controller
     public function destroy(Thread $thread)
     {
         //
+    }
+
+    protected function getThreads($channel, $filters)
+    {
+        $threads = Thread::latest()->filter($filters);
+
+        if ($channel->exists) {
+            $threads->where('channel_id', $channel->id);
+        }
+
+        $threads = $threads->get();
+
+        return $threads;
     }
 }
--- app/Thread.php
+++ app/Thread.php
@@ -2,42 +2,44 @@
 
 namespace App;
 
-use App\Reply;
-use App\Channel;
 use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
-{ 
-
+{
     protected $guarded = [];
-    
+
     /**
      * Fetch a path to the current thread.
-     * 
+     *
      * @return string
      */
     public function path()
     {
-      return "/threads/{$this->channel->slug}/{$this->id}";
+        return "/threads/{$this->channel->slug}/{$this->id}";
     }
 
     public function replies()
     {
-      return $this->hasMany(Reply::class);
+        return $this->hasMany(Reply::class);
     }
 
     public function creator()
     {
-      return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function channel()
     {
-      return $this->belongsTo(Channel::class);
+        return $this->belongsTo(Channel::class);
     }
 
     public function addReply($reply)
     {
         $this->replies()->create($reply);
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+        return $filters->apply($query);
     }
 }
