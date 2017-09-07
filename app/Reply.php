6d644855a3bf42ecf33828a8050ecaca952f1696
najibu <ns.najibu@gmail.com>
2017-09-07 16:50:08 +0100

    The Favorite Button

--- app/Reply.php
+++ app/Reply.php
@@ -31,4 +31,9 @@ class Reply extends Model
             return $this->favorites()->create($attributes);
         }
     }
+
+    public function isFavorited()
+    {
+        return $this->favorites()->where('user_id', auth()->id())->exists();
+    }
 }
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,24 @@
 <div class="panel panel-default">
   <div class="panel-heading">
-    <a href="#">
-      {{$reply->owner->name}}
-    </a> said {{$reply->created_at->diffForHumans()}}...
+    <div class="level">
+        <h5 class="flex">
+            <a href="#">
+              {{$reply->owner->name}}
+            </a> said {{$reply->created_at->diffForHumans()}}...
+        </h5>
+
+        <div>
+
+            <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary" {{ $reply->isFavorited() ? 'disabled': '' }}>
+                        {{ $reply->favorites()->count() }} {{ str_plural('Favorite', $reply->favorites()->count()) }}
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
   </div>
   <div class="panel-body">
     {{ $reply->body }}
