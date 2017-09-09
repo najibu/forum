729c634021e607b17935c12f8e19e05d31837ee1
najibu <ns.najibu@gmail.com>
2017-09-09 10:23:49 +0100

    installed barryvdh debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
     }
 }
