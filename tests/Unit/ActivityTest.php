78eaffdfc948162415877c63748dc1e372729613
najibu <ns.najibu@gmail.com>
2017-10-15 13:05:12 +0100

    How to Construct an Activity Feed with TDD

---
+++ tests/Unit/ActivityTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Activity;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ActivityTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test  */
+    public function it_records_activity_when_a_thread_is_created()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $this->assertDatabaseHas('activities', [
+            'type' => 'created_' . strtolower((new \ReflectionClass($thread))->getShortName()),
+            'user_id' => auth()->id(),
+            'subject_id' => $thread->id,
+            'subject_type' => get_class($thread)
+        ]);
+
+        $activity = Activity::first();
+
+        $this->assertEquals($activity->subject->id, $thread->id);
+    }
+
+    /** @test  */
+    public function it_records_activity_when_a_reply_is_created()
+    {
+        $this->signIn();
+
+        $reply = create('App\Reply');
+
+        $this->assertEquals(2, Activity::count());
+    }
+}
