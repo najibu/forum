01553ad044af986375f21166c8c8b760ac37df47
najibu <ns.najibu@gmail.com>
2017-11-12 10:21:06 +0000

    29 Flash Messaging With Vue

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -7,21 +7,20 @@ use Illuminate\Http\Request;
 
 class RepliesController extends Controller
 {
-
     public function __construct()
     {
-      $this->middleware('auth');
+        $this->middleware('auth');
     }
-    
+
     public function store($channelId, Thread $thread)
     {
-      $this->validate(request(), ['body' => 'required']);
-      
-      $thread->addReply([
+        $this->validate(request(), ['body' => 'required']);
+
+        $thread->addReply([
         'body' => request('body'),
         'user_id' => auth()->id()
       ]);
 
-      return back();
+        return back()->with('flash', 'Your reply has been left.');
     }
 }
