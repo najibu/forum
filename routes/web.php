debe6622a86f5c1c3d3f2e761af4df0eeea39ae6
najibu <ns.najibu@gmail.com>
2017-12-26 13:55:27 +0000

    Thread Subscriptions: Part 2 Features test

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/home', 'HomeController@index');
 
-// Threads
+// ThreadsController
 Route::get('threads', 'ThreadsController@index');
 Route::get('threads/create', 'ThreadsController@create');
 Route::get('threads/{channel}/{thread}', 'ThreadsController@show');
@@ -27,8 +27,11 @@ Route::delete('threads/{channel}/{thread}', 'ThreadsController@destroy');
 Route::post('threads', 'ThreadsController@store');
 Route::get('threads/{channel}', 'ThreadsController@index');
 
+// ThreadSubscriptionsController
+Route::post('/threads/{channel}/{thread}/subscriptions', 'ThreadSubscriptionsController@store')->middleware('auth');
 
-//Replies
+
+// RepliesController
 Route::post('threads/{channel}/{thread}/replies', 'RepliesController@store');
 Route::get('threads/{channel}/{thread}/replies', 'RepliesController@index');
 Route::patch('/replies/{reply}', 'RepliesController@update');
@@ -36,9 +39,9 @@ Route::delete('/replies/{reply}', 'RepliesController@destroy');
 
 // Channels
 
-// Favorites
+// FavoritesController
 Route::post('replies/{reply}/favorites', 'FavoritesController@store');
 Route::delete('replies/{reply}/favorites', 'FavoritesController@destroy');
 
-// Profiles
+// ProfilesController
 Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
