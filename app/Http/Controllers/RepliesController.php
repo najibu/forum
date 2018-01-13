2aa7b90a6e67e1da989312f4164923ee115ecd15
najibu <ns.najibu@gmail.com>
2018-01-13 13:29:54 +0000

    Don't Forget to Scan Your Files

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -51,16 +51,9 @@ class RepliesController extends Controller
     {
         $this->authorize('update', $reply);
 
-        try {
-            $this->validate(request(), ['body' => 'required|spamfree']);
+        $this->validate(request(), ['body' => 'required|spamfree']);
 
-            $reply->update(request(['body']));
-        } catch (\Exception $e) {
-            return response(
-                'Sorry,  your reply could not be saved at this time',
-                422
-            );
-        }
+        $reply->update(request(['body']));
     }
 
     /**
