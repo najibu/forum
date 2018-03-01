5a30a95e5bd5e281852f8b34312dbb43dc2d38a2
najibu <ns.najibu@gmail.com>
2018-03-01 13:26:24 +0000

    Trending Threads With Redis

---
+++ tests/Feature/TrendingThreadsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Redis;
+use Tests\TestCase;
+
+class TrendingThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        Redis::del('trending_threads');
+    }
+
+    /** @test  */
+    public function it_increments_a_thread_score_each_time_it_is_read()
+    {
+        $this->assertEmpty(Redis::zrevrange('trending_threads', 0, -1));
+
+        $thread = create('App\Thread');
+
+        $this->call('GET', $thread->path());
+
+        $trending = Redis::zrevrange('trending_threads', 0, -1);
+
+        $this->assertCount(1, $trending);
+
+        $this->assertEquals($thread->title, json_decode($trending[0])->title);
+    }
+}
